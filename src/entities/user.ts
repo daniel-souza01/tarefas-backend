486f35da932e9d5a8d2188b3203a3f771a424d06
Daniel Souza <danielsouza51764@gmail.com>
2024-01-16 15:58:23 -0300

    refactor user entity

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -29,24 +29,12 @@ export class User {
     return this.props.name
   }
 
-  public set name(name: string) {
-    this.props.name = name
-  }
-
   public get email(): string {
     return this.props.email
   }
 
-  public set email(email: string) {
-    this.props.email = email
-  }
-
   public get password(): UserPassword {
     return this.props.password
-  }
-
-  public set password(password: UserPassword) {
-    this.props.password = password
   }
 
   public get createdAt(): Date {
