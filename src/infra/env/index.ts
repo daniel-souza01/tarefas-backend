6e59c467a00fc606051e89e56f959cc54d3b232d
Daniel Souza <danielsouza51764@gmail.com>
2024-01-17 10:35:59 -0300

    add prisma and docker compose

--- src/infra/env/index.ts
+++ src/infra/env/index.ts
@@ -2,7 +2,13 @@ import 'dotenv/config'
 import { z } from 'zod'
 
 const envSchema = z.object({
+  NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
   PORT: z.coerce.number().default(3333),
+  DATABASE_URL: z.string(),
+  POSTGRES_PORT: z.string(),
+  POSTGRES_USER: z.string(),
+  POSTGRES_PASSWORD: z.string(),
+  POSTGRES_DB: z.string(),
 })
 
 const _env = envSchema.safeParse(process.env)
